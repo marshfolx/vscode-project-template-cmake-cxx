dce35fa2bc475eb999463ecd1517b5329d8ae2b2
chris percent <chris_percent@outlook.com>
2021-07-30 16:42:45 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*  Notice:
+    Press F7 to build with cmake.
+    Press F5 to run executable defined in CMakeLists.txt.
+    Cmake project name is automatically set as folder name.
+*/
+
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
